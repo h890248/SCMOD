02146c059be245840e79a8fb51d5b3eea3419479
halbino <h890248@gmail.com>
2016-12-11 00:41:58 -0200

    Criptografia class Validado com teste

--- ProjetoIntegrado/criptografia.cs
+++ ProjetoIntegrado/criptografia.cs
@@ -23,12 +23,19 @@ namespace ProjetoIntegrado
                 {
                     if ((chaveBase.IndexOf(letra) + pulo) > 36)
                     {
-                        pulo -= 37;
+                        int pulo_tmp = (pulo + chaveBase.IndexOf(letra)) -37;
+                        char letra_nova = chaveBase[pulo_tmp];
+                        Console.WriteLine("letra_nova == " + letra_nova);
+                        Console.WriteLine("array_texto[i] == " + array_texto[i]);
+                        array_texto[i] = letra_nova;
                     }
-                    char letra_nova= chaveBase[chaveBase.IndexOf(letra) + pulo];
-                    Console.WriteLine("letra_nova == " + letra_nova);
-                    Console.WriteLine("array_texto[i] == " + array_texto[i]);
-                    array_texto[i] = letra_nova;
+                    else
+                    {
+                        char letra_nova = chaveBase[chaveBase.IndexOf(letra) + pulo];
+                        Console.WriteLine("letra_nova == " + letra_nova);
+                        Console.WriteLine("array_texto[i] == " + array_texto[i]);
+                        array_texto[i] = letra_nova;
+                    }
                 }
             }
             text = new string(array_texto);
@@ -46,18 +53,26 @@ namespace ProjetoIntegrado
             int i = -1;
             foreach (char letra in array_texto)
             {
-                Console.WriteLine("lll == " + letra);
+                Console.WriteLine("letra == " + letra);
                 i++;
                 if (chaveBase.IndexOf(letra) > -1)
                 {
-                    if ((chaveBase.IndexOf(letra) + pulo) > 36)
+                    Console.WriteLine("chaveBase.IndexOf(letra) - pulo === ", (chaveBase.IndexOf(letra) - pulo));
+                    if ((chaveBase.IndexOf(letra) - pulo) < 0)
                     {
-                        pulo -= 37;
+                        int pulo_tmp = pulo - chaveBase.IndexOf(letra);
+                        char letra_nova = chaveBase[37 - pulo_tmp];
+                        Console.WriteLine("letra_nova == " + letra_nova);
+                        Console.WriteLine("array_texto[i] == " + array_texto[i]);
+                        array_texto[i] = letra_nova;
                     }
-                    char letra_nova = chaveBase[chaveBase.IndexOf(letra) - pulo];
-                    Console.WriteLine("letra_nova == " + letra_nova);
-                    Console.WriteLine("array_texto[i] == " + array_texto[i]);
-                    array_texto[i] = letra_nova;
+                    else
+                    {
+                        char letra_nova = chaveBase[chaveBase.IndexOf(letra) - pulo];
+                        Console.WriteLine("letra_nova == " + letra_nova);
+                        Console.WriteLine("array_texto[i] == " + array_texto[i]);
+                        array_texto[i] = letra_nova;
+                    }
                 }
             }
             code = new string(array_texto);
