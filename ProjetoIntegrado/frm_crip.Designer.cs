0dec4c4eec38fb38ca39c3dcc56ca0b00cea5f78
halbino <h890248@gmail.com>
2016-12-11 01:22:53 -0200

    RF-9 Completo

--- ProjetoIntegrado/frm_crip.Designer.cs
+++ ProjetoIntegrado/frm_crip.Designer.cs
@@ -36,21 +36,21 @@
             // 
             // btn_crip
             // 
-            this.btn_crip.Location = new System.Drawing.Point(346, 296);
+            this.btn_crip.Location = new System.Drawing.Point(231, 296);
             this.btn_crip.Name = "btn_crip";
-            this.btn_crip.Size = new System.Drawing.Size(75, 23);
+            this.btn_crip.Size = new System.Drawing.Size(190, 23);
             this.btn_crip.TabIndex = 2;
-            this.btn_crip.Text = "Criptografar";
+            this.btn_crip.Text = "Salvar Texto o Encriptografando";
             this.btn_crip.UseVisualStyleBackColor = true;
             this.btn_crip.Click += new System.EventHandler(this.btn_crip_Click);
             // 
             // btn_abrir
             // 
-            this.btn_abrir.Location = new System.Drawing.Point(265, 296);
+            this.btn_abrir.Location = new System.Drawing.Point(12, 296);
             this.btn_abrir.Name = "btn_abrir";
-            this.btn_abrir.Size = new System.Drawing.Size(75, 23);
+            this.btn_abrir.Size = new System.Drawing.Size(165, 23);
             this.btn_abrir.TabIndex = 3;
-            this.btn_abrir.Text = "Abrir";
+            this.btn_abrir.Text = "Abrir Arquivo Criptografado";
             this.btn_abrir.UseVisualStyleBackColor = true;
             this.btn_abrir.Click += new System.EventHandler(this.btn_abrir_Click);
             // 
@@ -68,6 +68,7 @@
             // rtb_crip
             // 
             this.rtb_crip.Location = new System.Drawing.Point(12, 49);
+            this.rtb_crip.MaxLength = 140;
             this.rtb_crip.Name = "rtb_crip";
             this.rtb_crip.Size = new System.Drawing.Size(415, 241);
             this.rtb_crip.TabIndex = 26;
--- ProjetoIntegrado/frm_crip.cs
+++ ProjetoIntegrado/frm_crip.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace ProjetoIntegrado
 {
     public partial class frm_crip : Form
     {
+        string arquivo;
+
         public frm_crip()
         {
             InitializeComponent();
@@ -19,14 +22,74 @@ namespace ProjetoIntegrado
 
         private void btn_crip_Click(object sender, EventArgs e)
         {
-            criptografia crip = new criptografia();
-            rtb_crip.Text = crip.toCode(rtb_crip.Text);
+            if (rtb_crip.Text != "")
+            {
+                criptografia crip = new criptografia();
+
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Title = "Abrir Arquivo Criptografado";
+                    saveFileDialog.InitialDirectory = @"c:\Program Files";
+                    saveFileDialog.Filter = "txt files (*.txt)|*.txt";
+                    saveFileDialog.RestoreDirectory = true;
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        arquivo = saveFileDialog.FileName;
+                    }
+                }
+                if (String.IsNullOrEmpty(arquivo))
+                {
+                    MessageBox.Show("Arquivo Invalido", "Aviso", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    try
+                    {
+                        File.WriteAllText(arquivo, crip.toCode(rtb_crip.Text));
+                        MessageBox.Show("Salvo com Sucesso", "Aviso", MessageBoxButtons.OK);
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+                rtb_crip.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Campo de texto não pode estar Nulo !!!");
+            }
         }
 
         private void btn_abrir_Click(object sender, EventArgs e)
         {
             criptografia crip = new criptografia();
-            rtb_crip.Text = crip.toText(rtb_crip.Text);
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Salvar Arquivo Criptografado";
+                openFileDialog.InitialDirectory = @"c:\Program Files"; 
+                openFileDialog.Filter = "txt files (*.txt)|*.txt"; 
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    arquivo = openFileDialog.FileName; 
+                }
+            }
+            if (String.IsNullOrEmpty(arquivo)) 
+            {
+                MessageBox.Show("Arquivo Invalido", "Aviso", MessageBoxButtons.OK);
+            }else{
+                using (StreamReader linha = new StreamReader(arquivo))
+                {
+                   
+                        string file_cri = linha.ReadToEnd();
+                        rtb_crip.Text = crip.toText(file_cri);
+                }
+            }
         }
     }
 }
